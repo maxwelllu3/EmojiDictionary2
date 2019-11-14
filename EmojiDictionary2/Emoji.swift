cf2e59cca7672bd767472b5d10fbe13bbb40d624
Maxwell Lu <blu@lcs.on.ca>
2019-11-13 21:01:43 -0500

    Got a base for the app ready

---
+++ EmojiDictionary2/Emoji.swift
@@ -0,0 +1,18 @@
+//
+//  Emoji.swift
+//  EmojiDictionary2
+//
+//  Created by Maxwell Lu on 2019-11-13.
+//  Copyright © 2019 Maxwell Lu. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
+//var example = Emoji(symbol: "🐹", name: "Hamster", description: "A cute animal, usually viewed as a household pet.", usage: "a pet")
